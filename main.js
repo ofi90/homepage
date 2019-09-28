93489b87af5a0cd6c0986aa6ad9d3b6fbab98b65
Bartosz Oficjalski <oficjalski90@gmail.com>
2019-09-29 01:39:20 +0200

    refactoring code

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ console.log(age);
 
 console.log(`Nazywam sie ${name} i mam ${age}`)
 
+const about = document.querySelector('.about__paragraph--js');
+
+console.log(about.innerHTML);
+
+about.innerHTML = '<strong>Pozdro</strong> JS';
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+console.log(paragraphs[0]);
